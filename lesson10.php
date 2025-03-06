911a7874a41c0ab4248b8d1e8247403a4ba4e2f9
okamoto702 <gangbenda2@gmail.com>
2025-03-07 00:14:05 +0900

    lesson10.php

--- lesson10.php
+++ lesson10.php
@@ -1,1 +1,16 @@
-﻿<?php
+<?php
+// 点数を格納
+$scores = [10, 60, 90, 70, 50];
+
+foreach ($scores as $score) {
+    if ($score > 80) {
+        echo "{$score}点は優です。\n";
+    } elseif ($score > 60) {
+        echo "{$score}点は良です。\n";
+    } elseif ($score > 40) {
+        echo "{$score}点は可です。\n";
+    } else {
+        echo "{$score}点は不可です。\n";
+    }
+}
+?>
